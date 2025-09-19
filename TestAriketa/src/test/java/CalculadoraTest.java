9bba518bbe4aa616b40070866a8b5ec9bc9c65b0
ihernandezgoo <ihernandezgo25paag@ikzubirimanteo.com>
2025-09-19 14:46:49 +0200

    Ariketa bukatua

--- TestAriketa/src/test/java/CalculadoraTest.java
+++ TestAriketa/src/test/java/CalculadoraTest.java
@@ -6,12 +6,12 @@ public class CalculadoraTest {
     @Test
     public void testSumaAcierta() {
         Calculadora calc = new Calculadora();
-        assertEquals(5, calc.suma(2, 2));
+        assertEquals(5, calc.suma(2, 3));    
     }
 
-    @Test
-    public void testSumaAcierta() {
+   @Test
+    public void testSumaFalla() {
         Calculadora calc = new Calculadora();
-        assertEquals(5, calc.suma(2, 3));
+        assertEquals(5, calc.suma(2, 2));    
     }
 }
