6918ff9169756ade4cc2887eee90a8e542767000
ihernandezgoo <ihernandezgo25paag@ikzubirimanteo.com>
2025-09-25 14:35:02 +0200

    prueba 3

--- TestAriketa/src/main/java/Calculadora.java
+++ TestAriketa/src/main/java/Calculadora.java
@@ -2,10 +2,4 @@ public class Calculadora {
     public int suma(int a, int b) {
         return a + b;
     }
-
-    public static void main(String[] args) {
-        Calculadora calc = new Calculadora();
-        int resultado = calc.suma(2, 3);
-        System.out.println("La suma es: " + resultado);
-    }
 }
